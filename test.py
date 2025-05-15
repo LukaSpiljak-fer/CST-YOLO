8ed4c46adab2dee88ef8c43efc7a6712bd9fbc9d
LukaSpiljak-fer <luka.spiljak@fer.unizg.hr>
2025-05-15 10:40:37 +0200

    fixed test.py

--- test.py
+++ test.py
@@ -88,7 +88,7 @@ def test(data,
             model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
         task = opt.task if opt.task in ('train', 'val', 'test') else 'val'  # path to train/val/test images
  
-        prefix = colorstr(f'{task}: ')
+        prefix = colorstr(f"{task}: ")
         dataloader = create_dataloader(data[task], imgsz, batch_size, gs, opt, pad=0.5,   rect=True,  
                                        prefix=prefix)[0]
 

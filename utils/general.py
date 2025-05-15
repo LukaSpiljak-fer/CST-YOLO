fc3c5281711e135cdbe8010626f334cccbe0b18b
LukaSpiljak-fer <luka.spiljak@fer.unizg.hr>
2025-05-15 10:55:11 +0200

    fixed general.py/colorstr

--- utils/general.py
+++ utils/general.py
@@ -189,28 +189,21 @@ def one_cycle(y1=0.0, y2=1.0, steps=100):
 
 
 def colorstr(*input):
-    # Colors a string https://en.wikipedia.org/wiki/ANSI_escape_code, i.e.  colorstr('blue', 'hello world')
-    *args, string = input if len(input) > 1 else ('blue', 'bold', input[0])  # color arguments, string
-    colors = {'black': '\033[30m',  # basic colors
-              'red': '\033[31m',
-              'green': '\033[32m',
-              'yellow': '\033[33m',
-              'blue': '\033[34m',
-              'magenta': '\033[35m',
-              'cyan': '\033[36m',
-              'white': '\033[37m',
-              'bright_black': '\033[90m',  # bright colors
-              'bright_red': '\033[91m',
-              'bright_green': '\033[92m',
-              'bright_yellow': '\033[93m',
-              'bright_blue': '\033[94m',
-              'bright_magenta': '\033[95m',
-              'bright_cyan': '\033[96m',
-              'bright_white': '\033[97m',
-              'end': '\033[0m',  # misc
-              'bold': '\033[1m',
-              'underline': '\033[4m'}
+    # Colors a string using ANSI escape codes
+    if len(input) > 1:
+        args, string = input[:-1], input[-1]  # Extract colors + string
+    else:
+        args, string = ('blue', 'bold'), input[0]  # Default colors
+    
+    colors = {  # ANSI color codes
+        'black': '\033[30m', 'red': '\033[31m', 'green': '\033[32m',
+        'yellow': '\033[33m', 'blue': '\033[34m', 'magenta': '\033[35m',
+        'cyan': '\033[36m', 'white': '\033[37m', 'bold': '\033[1m',
+        'underline': '\033[4m', 'end': '\033[0m'
+    }
+    
     return ''.join(colors[x] for x in args) + f'{string}' + colors['end']
+
 
 
 def labels_to_class_weights(labels, nc=80):
